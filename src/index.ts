5c9bc47c37fbf5e84ec9e4b0af9c62995f2123f5
afeiship <1290657123@qq.com>
2021-05-21 22:29:05 +0800

    feat: update response type

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import nxFetchWithTimeout from '@jswork/next-fetch-with-timeout';
 import nxFetchWithDelay from '@jswork/next-fetch-with-delay';
 import nxFetchWithRandomUa from '@jswork/next-fetch-with-random-ua';
 import nxFetchWithProxy from '@jswork/next-fetch-with-proxy';
+import nxFetchWithResponseType from '@jswork/next-fetch-with-response-type';
 import nxApplyFetchMilddeware from '@jswork/next-apply-fetch-middleware';
 import nodeFetch from 'node-fetch';
 
@@ -14,6 +15,7 @@ export default (inConfig, inOptions?) => {
     nxFetchWithDelay,
     nxFetchWithRandomUa,
     nxFetchWithProxy,
+    nxFetchWithResponseType,
     nxFetchWithDebug
   ])(nodeFetch);
 
