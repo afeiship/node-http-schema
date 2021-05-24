268418347ff536a83251b67c2ffae993f4841cbe
afeiship <1290657123@qq.com>
2021-05-24 17:22:32 +0800

    fix: remove response-type from node-schema

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,6 @@ import nxFetchWithTimeout from '@jswork/next-fetch-with-timeout';
 import nxFetchWithDelay from '@jswork/next-fetch-with-delay';
 import nxFetchWithRandomUa from '@jswork/next-fetch-with-random-ua';
 import nxFetchWithProxy from '@jswork/next-fetch-with-proxy';
-import nxFetchWithResponseType from '@jswork/next-fetch-with-response-type';
 import nxApplyFetchMilddeware from '@jswork/next-apply-fetch-middleware';
 import nodeFetch from 'node-fetch';
 
@@ -15,7 +14,6 @@ export default (inConfig, inOptions?) => {
     nxFetchWithDelay,
     nxFetchWithRandomUa,
     nxFetchWithProxy,
-    nxFetchWithResponseType,
     nxFetchWithDebug
   ])(nodeFetch);
 
